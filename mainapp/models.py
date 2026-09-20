f99e084e262336f5c8210cf9d876711e77d1ce05
km46563 <sirkowi3@gmail.com>
2026-09-20 17:51:55 +0200

    Finished 1st version of models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,32 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.fields import CharField
-from django.db.models.functions import Now
 
 class User(AbstractUser):
-    class UserStatus(models.TextChoices):
-        ACTIVE = 'ACTIVE', 'Aktywny'
-        BLOCKED = 'BLOCKED', 'Zablokowany'
-
-    status = models.CharField(
-            max_length=10,
-            choices=UserStatus.choices,
-            default=UserStatus.ACTIVE
-            )
+    pass
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.CharField()
-    createdAt = models.DateTimeField(db_default=Now())
-    mood = CharField = models.CharField(max_length=50)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    mood = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="continuations")
 
     class PostType(models.TextChoices):
         STANDARD = 'STANDARD', 'Standardowy'
         LIMITED_COLLAB = 'LIMITED_COLLAB', 'Limitowana kolaboracja'
         UNLIMITED_COLLAB = 'UNLIMITED_COLLAB', 'Nielimitowana kolaboracja'
 
-    status = models.CharField(
+    post_type = models.CharField(
             max_length=20,
             choices=PostType.choices,
             default=PostType.STANDARD
@@ -47,10 +39,12 @@ class Post(models.Model):
             default=PostGenre.OTHER
             )
 
+
 class CollabRequest(models.Model):
-    createdAt = models.DateTimeField(db_default=Now())
-    resolvedAt = models.DateTimeField()
-    note = CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField(null=True, blank=True)
+    note = models.TextField(blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     class RequestStatus(models.TextChoices):
         PENDING = 'PENDING', 'Oczekujący',
@@ -63,13 +57,18 @@ class CollabRequest(models.Model):
             default=RequestStatus.PENDING
             )
 
+
 class PostReport(models.Model):
-    note = models.CharField()
-    createdAt = models.DateTimeField(db_default=Now())
+    note = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField(null=True, blank=True)
+    reported_post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_reports')
+    resolver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True related_name='resolved_post_reports')
     
     class ReportStatus(models.TextChoices):
         PENDING = 'PENDING', 'Oczekujący'
-        POST_SAVE = 'POST_SAVE', 'Post jest bezpieczny'
+        POST_SAFE = 'POST_SAFE', 'Post jest bezpieczny'
         POST_DELETION = 'POST_DELETION', 'Usunięto post'
         USER_BLOCKED = 'USER_BLOCKED', 'Zablokowano użytkownika'
 
@@ -79,13 +78,18 @@ class PostReport(models.Model):
             default=ReportStatus.PENDING
             )
 
+
 class UserReport(models.Model):
-    note = models.CharField()
-    createdAt = models.DateTimeField(db_default=Now())
+    note = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField(null=True, blank=True)
+    reported_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reports')
+    resolver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='resolved_user_reports')
     
     class ReportStatus(models.TextChoices):
         PENDING = 'PENDING', 'Oczekujący'
-        USER_SAVE = 'USER_SAVE', 'Użytkownik jest bezpieczny'
+        USER_SAFE = 'USER_SAFE', 'Użytkownik jest bezpieczny'
         USER_BLOCKED = 'USER_BLOCKED', 'Zablokowano użytkownika'
 
     status = models.CharField(
@@ -95,3 +99,68 @@ class UserReport(models.Model):
             )
 
 
+class Profile(models.Model):
+    description = models.TextField(blank=True)
+    # TODO: Here will be some kind of background; gradient or png/jpg, I don't know yet
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Follow(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["follower", "followed"], name="unique_follow"),
+            models.CheckConstraint(condition=~models.Q(follower=models.F("followed")), name="prevent_self_follow")
+        ]
+   
+
+class SavedPost(models.Model):
+    saved_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "post"], name="unique_save")
+        ]
+
+
+
+class Like(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "post"], name="unique_like")
+        ]
+
+
+class Comment(models.Model):
+    content = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+
+
+class FeedSession(models.Model):
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PostView(models.Model):
+    position = models.PositiveIntegerField()
+    viewed_at = models.DateTimeField(auto_now_add=True)
+    feed_session = models.ForeignKey(FeedSession, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['position', 'feed_session'], name='unique_position_in_feed_session')
+        ]
